3ff5cc154d85f052a3e641d112c6161903f54ac6
fe-jcorreia <fe.jcorreia@gmail.com>
2021-03-18 19:26:44 -0300

    components, sidebar and content added, useMovies hook added

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,30 @@
+import { useMovies } from "../hooks/useMovies";
+import { MovieCard } from "./MovieCard";
+
+
 export function Content() {
-  // Complete aqui
+  const { movies, selectedGenre } = useMovies();
+  
+  return (
+    <>
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
+import { useMovies } from "../hooks/useMovies";
+import { Button } from "./Button";
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, selectedGenreId, handleClickButton} = useMovies();
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
